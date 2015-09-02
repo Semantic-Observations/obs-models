ac8b497206c2ffcdf036aba3777fbe5c0d59a9e0
Bryce Mecum <petridish@gmail.com>
2015-09-02 12:26:41 -0800

    Expand reason.sh to do DL queries on A==B

--- examples/classify-non-hierarchical/reason.sh
+++ examples/classify-non-hierarchical/reason.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-owltools ./test-noequivclass.owl --reasoner hermit --run-reasoner
-owltools ./test-noequivclass-cardinality.owl --reasoner hermit --run-reasoner
-owltools ./test-equivclass.owl --reasoner hermit --run-reasoner
-owltools ./test-equivclass-cardinality.owl --reasoner hermit --run-reasoner
+owltools ./test-noequivclass.owl --reasoner hermit --run-reasoner --assert-implied --reasoner-query 'A'
+owltools ./test-noequivclass-cardinality.owl --reasoner hermit --run-reasoner --assert-implied --reasoner-query 'A'
+owltools ./test-equivclass.owl --reasoner hermit --run-reasoner --assert-implied --reasoner-query 'A'
+owltools ./test-equivclass-cardinality.owl --reasoner hermit --run-reasoner --assert-implied --reasoner-query 'A'
