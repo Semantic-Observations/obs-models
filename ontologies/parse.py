1e1c49cbaccbbf8348c20917393fe9b74a50d86a
Matt Jones <gitcode@magisa.org>
2015-09-29 12:40:32 -0800

    Example of how to serialize owl:unionOf as a series of Statements.

---
+++ ontologies/parse.py
@@ -0,0 +1,15 @@
+""" parse.py
+    Matt Jones (jones@nceas.ucsb.edu)
+    
+    Parse a Turtle file into a model
+"""
+
+import RDF
+
+if __name__ == "__main__":
+    parser=RDF.TurtleParser()
+    model = RDF.Model()
+    parser.parse_into_model(model, "file:///Users/jones/development/obs-models/ontologies/union.ttl")
+    print(model.size())
+    for statement in model:
+        print(statement)
